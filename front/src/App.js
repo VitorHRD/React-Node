9305d924cc7949e3146a20ab981cbc33f834abcc
VitorHRD <vrdev435@gmail.com>
2022-04-24 00:03:39 -0300

    origin

---
+++ front/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react"
+import './App.css';
+
+function App() {
+
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('/api/videos').then(res => res.json()).then(data => {
+      let dataReverse = data.reverse()
+      setVideos(dataReverse)
+    })
+  })
+  function AddLink() {
+    let value = document.getElementById("inputLink").value;
+    let findValue = videos.find((i)=>{
+      return i === value
+    })
+    if(value !== "" && findValue === undefined){
+    fetch(`/${value}`).then(res => res.json()).then(data => {
+      let dataReverse = data.reverse()
+      setVideos(dataReverse)
+    })
+    
+  }
+  else{
+    if(value === ""){
+      window.alert("Adicione um ID")
+    }
+    else{
+      window.alert("ID já adicionado")
+    }
+  }
+  }
+  function removeLink(id){
+    fetch(`/delete/${id}` ,{method:"DELETE"}).then(res=>res.json()).then(data=>{
+      let dataReverse = data.reverse()
+      setVideos(dataReverse)
+    })
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="divForm">
+          <form>
+            <input id="inputLink" key="AddLink" type="text" placeholder="Add video ID" ></input>
+          </form>
+
+          <button onClick={() => { AddLink() }}>Add</button>
+        </div>
+        {videos.map((id, index) => {
+          return (
+            <div className="videoDiv">
+              <a key={index} href={`https://youtube.com/watch?v=${id}`}><img alt={index} src={`https://img.youtube.com/vi/${id}/0.jpg`}></img></a>
+              <img  onClick={()=>{
+                removeLink(id)
+              }} alt={index}className="removeImg" title="Remove" src="remove.png" width="32px"></img>
+            </div>
+          )
+        })}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+const path = require("path")
+require('dotenv').config();
+
+const videos = []
+
+app.listen(3000, () => {
+    console.log("Running")
+})
+app.get('/api/videos', (req, res) => {
+    res.send(videos)
+})
+app.get('/:id',(req,res)=>{
+   let id = req.params.id
+   videos.push(id)
+   res.send(videos)
+})
+app.delete('/delete/:id',(req , res)=>{
+    let id = req.params.id
+    let index = videos.indexOf(id)
+    if(index > -1) {
+        videos.splice(index,1)
+      }
+      res.send(videos)
+})
+
+    app.use(express.static(path.join(__dirname, 'front/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'front/build/index.html', (error) => {
+            res.status(500).send(error)
+        }))
+    })
